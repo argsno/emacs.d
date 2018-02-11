ecafa0e7d9e289c50961feaba77cdf6e349f9df7
Andrew Wong <argsno@gmail.com>
2018-02-11 10:45:43 +0800

    Format evil

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,11 +1,11 @@
-(maybe-require-package 'evil)
-(evil-mode t)
+(when (maybe-require-package 'evil)
+  (evil-mode t))
 
-(maybe-require-package 'evil-leader)
-(global-evil-leader-mode)
-(evil-leader/set-leader ",")
-(evil-leader/set-key
-  "b" 'switch-to-buffer
-  "w" 'save-buffer)
+(when (maybe-require-package 'evil-leader)
+  (global-evil-leader-mode)
+  (evil-leader/set-leader ",")
+  (evil-leader/set-key
+    "b" 'switch-to-buffer
+    "w" 'save-buffer))
 
 (provide 'init-evil)
