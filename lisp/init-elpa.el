0963261ac8ac76535449e0eb2a2457da842c6101
Andrew Wong <argsno@gmail.com>
2018-02-11 14:10:21 +0800

    elpa config

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -16,17 +16,24 @@
        (proto (if no-ssl "http" "https")))
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
   ;; Official MELPA Mirror, in case necessary.
-  ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa-cn" (concat proto "://elpa.emacs-chain.org/melpa/")) t)
   (if (< emacs-major-version 24)
       ;; For important compatibility libraries like cl-lib
       (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))
+      (add-to-list 'package-archives '("gnu-cn" . (concat proto "://elpa.emacs-china.org/gnu/")))
     (unless no-ssl
       ;; Force SSL for GNU ELPA
-      (setcdr (assoc "gnu" package-archives) "https://elpa.gnu.org/packages/"))))
+      (setcdr (assoc "gnu" package-archives) "https://elpa.gnu.org/packages/")
+      (setcdr (assoc "gnu-cn" package-archives) "https://elpa.emacs-china.org/gnu/"))))
 
 ;; We include the org repository for completeness, but don't normally
 ;; use it.
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("org-cn" . "http://elpa.emacs-china.org/org/"))
+
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                         ("org" . "http://elpa.emacs-china.org/org/")
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")))
 
 
 
